f8c556f6769092b8f8b1b2c54e24b8fdd238e7b8
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-06 16:34:32 -0500

    Adding logo to navbar

--- bdg/src/app/components/nav.js
+++ bdg/src/app/components/nav.js
@@ -1,13 +1,19 @@
 import styles from './nav.module.css';
 import Link from 'next/link';
+import Image from 'next/image'
 
 function Nav() {
     return (
         <nav className='bg-black text-white'>
             <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-                <div className='flex items-center justify-between h-16'>
-                    <ul className='flex justify-between w-full'> 
-                        <li><Link href='/'>Home</Link></li>
+                <div className='flex justify-between h-18'>
+                    <ul className='flex justify-between w-full'>
+                        <li><Link href='/'>  <Image
+                            src='/bdg.png'
+                            width={100}
+                            height={100}
+                            alt="Bluegrass Developers Guild Logo"
+                        /></Link></li>
                         <li><Link href='/about'>About</Link></li>
                         <li><Link href='/companies'>Companies</Link></li>
                         <li><Link href='/bookclub'>Book Club</Link></li>
--- bdg/src/app/page.js
+++ bdg/src/app/page.js
@@ -4,15 +4,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <div>
-      <Image 
-        src='/bdg.png'
-        width={150}
-        height={150}
-        alt="Bluegrass Developers Guild Logo"
-      />
+     
       <Nav />
-      </div>
+      
       
       
 
