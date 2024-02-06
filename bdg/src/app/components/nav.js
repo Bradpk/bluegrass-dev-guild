2ec43a467cdcf08d3b9ea7b65738528bd56067c9
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-06 16:13:52 -0500

    Navbar layout

--- bdg/src/app/components/nav.js
+++ bdg/src/app/components/nav.js
@@ -4,13 +4,17 @@ import Link from 'next/link';
 function Nav() {
     return (
         <nav className='bg-black text-white'>
-            <ul>
-                <li><Link href='/'>Home</Link></li>
-                <li><Link href='/about'>About</Link></li>
-                <li><Link href='/companies'>Companies</Link></li>
-                <li><Link href='/bookclub'>Book Club</Link></li>
-                <li><Link href='/contact'>Join Us</Link></li>
-            </ul>
+            <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+                <div className='flex items-center justify-between h-16'>
+                    <ul className='flex justify-between w-full'> 
+                        <li><Link href='/'>Home</Link></li>
+                        <li><Link href='/about'>About</Link></li>
+                        <li><Link href='/companies'>Companies</Link></li>
+                        <li><Link href='/bookclub'>Book Club</Link></li>
+                        <li><Link href='/contact'>Join Us</Link></li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     );
 }
