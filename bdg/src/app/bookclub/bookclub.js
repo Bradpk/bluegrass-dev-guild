3abc16afda75243f358694e0cf0665f661feebad
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2024-02-06 15:41:58 -0500

    adding more info

--- bdg/src/app/bookclub/bookclub.js
+++ bdg/src/app/bookclub/bookclub.js
@@ -3,6 +3,10 @@ import styles from './bookclub.module.css';
 function BookClub() {
     return (
         <>
+        <p>Meetups are to learn things by practice<br></br>
+book club is to learn things by studying</p>
+<p>Our purpose is to find books the group will enjoy and read, and learn something. We want books that help us learn and grow as technology leaders.</p>
+<p>Below is our list of read and unread books. If you would like to take part, join our slack channel.</p>
         </>
     );
 }
--- bdg/src/app/companies/companies.js
+++ bdg/src/app/companies/companies.js
@@ -3,6 +3,7 @@ import styles from './companies.module.css';
 function Companies() {
     return (
         <>
+        <p>The bluegrass has many talented developers working for some great companies locally and remote.</p>
         </>
     );
 }
